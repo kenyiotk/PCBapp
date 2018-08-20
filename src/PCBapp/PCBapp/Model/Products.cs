37631675bbe4edc9ac041c4452d83d50da03819a
kenyiotk <i1410994@continental.edu.pe>
2018-08-20 17:33:04 -0500

    submit 2

--- src/PCBapp/PCBapp/Model/Products.cs
+++ src/PCBapp/PCBapp/Model/Products.cs
@@ -10,6 +10,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+using RestSharp;
+using RestSharp.Deserializers;
+using SimpleJson;
+
+
 namespace PCBapp.Model
 {
     public class Products
@@ -18,17 +23,99 @@ namespace PCBapp.Model
         public string first_name { get; set; }
         public string last_name { get; set; }
         public string avatar { get; set; }
-        public static List<Product> local()
+        public static List<Products> local()
         {
-            return new List<Product>() {
-                new Product(){first_name = "Uno" },
-                new Product(){first_name = "Dos" },
-                new Product(){first_name = "Tres" },
-                new Product(){first_name = "Cuatro" },
-                new Product(){first_name = "Cinco" },
-                new Product(){first_name = "Seis" },
-                new Product(){first_name = "Siete" },
-                new Product(){first_name = "Ocho" },
+            return new List<Products>() {
+                new Products(){first_name = "Uno" },
+                new Products(){first_name = "Dos" },
+                new Products(){first_name = "Tres" },
+                new Products(){first_name = "Cuatro" },
+                new Products(){first_name = "Cinco" },
+                new Products(){first_name = "Seis" },
+                new Products(){first_name = "Siete" },
+                new Products(){first_name = "Ocho" },
             };
         }
+        public static RestClient get(int id, Func<Products, int> toDo)
+        {
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://reqres.in/");
+
+            var request = new RestRequest();
+            request.Resource = String.Format("api/users/{0}", id);
+
+            IRestResponse response = client.Execute(request);
+            ProductObjectRest rootObject = null;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+                rootObject = deserial.Deserialize<ProductObjectRest>(response);
+                //return rootObject.data;
+            }
+            else
+            {
+                rootObject = new ProductObjectRest();
+                //return rootObject.data;
+            }
+
+            toDo(rootObject.data);
+            return client;
+        }
+        public static List<Products> sync()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://reqres.in/");
+
+            var request = new RestRequest();
+            request.Resource = "api/users";
+
+            IRestResponse response = client.Execute(request);
+            ProductRest rootObject;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+                rootObject = deserial.Deserialize<ProductRest>(response);
+                //return rootObject.data;
+            }
+            else
+            {
+                rootObject = new ProductRest();
+                //return rootObject.data;
+            }
+            return rootObject.data;
+        }
+        public static RestClient sync(Func<List<Products>, int> toDo)
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://reqres.in/");
+
+            var request = new RestRequest();
+            request.Resource = "api/users";
+
+            IRestResponse response = client.Execute(request);
+            ProductRest rootObject;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+                rootObject = deserial.Deserialize<ProductRest>(response);
+                //return rootObject.data;
+            }
+            else
+            {
+                rootObject = new ProductRest();
+                //return rootObject.data;
+            }
+            toDo(rootObject.data);
+            return client;
+        }
+    }
+    public class ProductRest
+    {
+        public List<Products> data { get; set; }
+    }
+    public class ProductObjectRest
+    {
+        public Products data { get; set; }
+    }
+
 }
