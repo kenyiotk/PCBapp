4b6f28d273ec5e161c40108a282f2f8bca67df1b
Alex Perez <i1123953@continental.edu.pe>
2018-08-20 19:32:25 -0500

    Final Con error

--- src/PCBapp/PCBapp/Activities/Activity_login.cs
+++ src/PCBapp/PCBapp/Activities/Activity_login.cs
@@ -15,12 +15,35 @@ namespace PCBapp.Activities
     [Activity(Label = "login_activity")]
     public class Activity_login : Activity
     {
+        Button btnlogin;
+        Button btnRegister;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.login_activity);
+            btnlogin = FindViewById<Button>(Resource.Id.button1);
+            btnlogin.Click += dologin;
 
+            btnRegister = FindViewById<Button>(Resource.Id.button2);
+            btnRegister.Click += doregister;
+        }
+        private void dologin(Object sender, EventArgs e)
+        {
+            bool valid = true;
+            if(valid)
+            {
+                Intent newActivity = new Intent(this, typeof(MainActivity));
+                this.StartActivity(newActivity);
+            }
+            else
+            {
 
+            }
+        }
+        private void doregister(object sender,EventArgs o)
+        {
+            Intent newActivity = new Intent(this, typeof(Activity_register));
+            this.StartActivity(newActivity);
         }
     }
 }
